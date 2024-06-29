1bdc0c433205ad793c31e636a4ae47fae2303778
mari <joelkerbusch@gmail.com>
2024-06-29 17:04:35 +0200

    added new tests

--- test/index.js
+++ test/index.js
@@ -13,16 +13,17 @@ let result = addon.primaryColorByImageUrl(
 
 console.log(result);
 
-let base64_img = get_image_from_fs();
+let base64_img = get_image_from_fs("images.jpeg");
+let base64_img1 = get_image_from_fs("c1ba9a1b65ba3e3f3cf0ca43cab00b1d.jpg")
 
-let result1 = addon.primaryColorByBase64(base64_img);
+let result1 = addon.primaryColorByBase64([base64_img, base64_img1]);
 
 console.log(result1);
 
-function get_image_from_fs() {
+function get_image_from_fs(image_string) {
     try {
 
-        let image = fs.readFileSync('./test/images.jpeg', 'base64');
+        let image = fs.readFileSync('./test/' + image_string, 'base64');
 
         return image;
 
